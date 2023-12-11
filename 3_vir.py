2036302e6bfafabb658ce03e54920b447a064c13
Arthur Buzelin <125529624+Buzelin2@users.noreply.github.com>
2023-12-11 01:05:45 -0300

    Update 3_vir.py

--- 3_vir.py
+++ 3_vir.py
@@ -17,9 +17,11 @@ pipeline = transformers.pipeline(
 
 
 template = """
-write another paragraph to continue the story that follows, keeping the style of the story's author, Virginia Woolf, a modernist English writer whose text was written in 1917:
-
 {text}
+
+write another paragraph to continue the story above, keeping the style of the story's author, Virginia Woolf, a modernist English writer whose text was written in 1917:
+
+
 """
 
 prompt = PromptTemplate(input_variables=["text"], template=template)

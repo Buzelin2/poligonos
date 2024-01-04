8b0bef9439630ea57f29dd8c128a0a9dd220fa8e
João Silva <buzelin2@gmail.com>
2024-01-03 21:03:35 -0300

    aaaa

--- eli/1_eli.py
+++ eli/1_eli.py
@@ -2,8 +2,6 @@ from transformers import AutoTokenizer
 import transformers
 import torch
 from langchain.prompts import PromptTemplate
-import sys
-
 
 model = "meta-llama/Llama-2-7b-chat-hf"
 
@@ -15,29 +13,28 @@ pipeline = transformers.pipeline(
     device_map="auto",
 )
 
-
 template = """
 {text}
 
 write another paragraph to continue the story above, keeping the style of the story's author, Elizabeth Bowen:
-
-
 """
 
 prompt = PromptTemplate(input_variables=["text"], template=template)
 
-with open('eli.txt', 'r') as poem:
-    text = poem.read()
+for i in range(11):  # Loop para iterar sobre os arquivos
+    file_name = f'eli{i}.txt'
+    with open(file_name, 'r') as poem:
+        text = poem.read()
 
-poem_prompt = prompt.format(text=text)
+    poem_prompt = prompt.format(text=text)
 
-sequences = pipeline(
-    poem_prompt,
-    do_sample=True,
-    top_k=10,
-    num_return_sequences=1,
-    eos_token_id=tokenizer.eos_token_id
-)
+    sequences = pipeline(
+        poem_prompt,
+        do_sample=True,
+        top_k=10,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id
+    )
 
-for seq in sequences:
-    print(f"Result: {seq['generated_text']}")
+    for seq in sequences:
+        print(f"Result for {file_name}: {seq['generated_text']}")
